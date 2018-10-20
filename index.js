0f07112d1077d1fbfb95bfaaf9b8aa34b04f9d8d
Виталий Кулешин <antivitla@gmail.com>
2018-10-20 17:30:39 +0300

    do not read config until init

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const storage = require('minota-storage');
-const config = require('minota-shared/config').read();
+const config = require('minota-shared/config');
 
+let storageConfig; // will read at init time
 const server = express();
+
+function init (userStorageConfig) {
+  storageConfig = userStorageConfig || config.read().storage;
+  return server;
+}
 
 server.use(bodyParser.json());
 server.use(cors());
@@ -14,25 +20,25 @@ server.get('/', (req, res) => res.send('Welcome to Minota server'));
 
 // Get notes
 server.get('/last', (req, res) => storage
-  .config(config.storage)
+  .config(storageConfig)
   .get({ last: true })
   .then(notes => res.send(notes))
   .catch(error => res.status(400).send(error.message)));
 
 server.get('/last/day', (req, res) => storage
-  .config(config.storage)
+  .config(storageConfig)
   .get({ last: 'day' })
   .then(notes => res.send(notes))
   .catch(error => res.status(400).send(error.message)));
 
 server.get('/topics/all', (req, res) => storage
-  .config(config.storage)
+  .config(storageConfig)
   .get({ topics: 'all' })
   .then(topics => res.send(topics))
   .catch(error => res.status(400).send(error.message)));
 
 server.get('/notes/all', (req, res) => storage
-  .config(config.storage)
+  .config(storageConfig)
   .get({ notes: 'all' })
   .then(notes => res.send(notes))
   .catch(error => res.status(400).send(error.message)));
@@ -43,7 +49,7 @@ server.get('/notes', (req, res) => {
     Object.assign(params, { searchBy: 'topic', topic: req.query.topic });
   }
   return storage
-    .config(config.storage)
+    .config(storageConfig)
     .get(params)
     .then(notes => res.send(notes))
     .catch(error => res.status(400).send(error.message));
@@ -51,9 +57,9 @@ server.get('/notes', (req, res) => {
 
 // Save notes
 server.post('/content/:id', (req, res) => storage
-  .config(config.storage)
+  .config(storageConfig)
   .post({ notes: Array.isArray(req.body) ? req.body : [req.body] })
   .then(() => res.send([req.body]))
   .catch(error => res.status(500).send(error.message)));
 
-module.exports = server;
+module.exports = { init };
